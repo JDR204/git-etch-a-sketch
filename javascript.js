7f5a21d166b4f317f6d95f0252da7eab0d6c4e30
JDR204 <jreynolds204@yahoo.com>
2023-09-15 02:41:11 -0500

    Add new grid generator

--- javascript.js
+++ javascript.js
@@ -4,6 +4,7 @@ let gridSize = 16;
 
 for (let i = 0; i < gridSize; i++) {
     const column = document.createElement("div");
+    column.classList = "column";
     for (let j = 0; j < gridSize; j++) {
         const box = document.createElement("div");       
         box.classList = "box";
@@ -16,10 +17,34 @@ const boxes = document.querySelectorAll(".box");
 boxes.forEach((div) => {
         div.addEventListener("mouseover", e => {
             e.target.classList.add("hoverEffect");
-            console.log(e);
     });
-        div.addEventListener("mouseout", e => {
-            e.target.classList.remove("hoverEffect");
-            console.log(e);
+});
+
+const topButton = document.querySelector("#topButton button");
+topButton.addEventListener("click", e => {
+    let newSize = prompt("New grid size? (Max: 100)");
+    if (newSize > 0 && newSize <= 100) {
+        let columns = document.querySelectorAll(".column");
+        columns.forEach((div) => {
+            gridContainer.removeChild(div);
         });
+        for (let i = 0; i < newSize; i++) {
+            const column = document.createElement("div");
+            column.classList = "column";
+            for (let j = 0; j < newSize; j++) {
+                const box = document.createElement("div");       
+                box.classList = "box";
+                gridContainer.appendChild(column);
+                column.appendChild(box);
+            }
+        }
+        const boxes = document.querySelectorAll(".box");
+        boxes.forEach((div) => {
+            div.addEventListener("mouseover", e => {
+                e.target.classList.add("hoverEffect");
+            });
+        });
+    }   else {
+        alert("Error");
+    }
 });
